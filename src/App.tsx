186f6a7ea3326e5676444b63b087091c6ff09198
Manish Kumar Sharma <manishk_sharma@outlook.com>
2023-12-24 12:15:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+// import { Inter } from 'next/font/google'
+
+// const inter = Inter({ subsets: ['latin'] });
+
+function App() {
+  return (
+
+      <h1 className="bg-black">
+      
+      Hello world!
+    </h1>
+  );
+}
+
+export default App;
